5616be7861338526de942e4554901d12d35e2bb7
kobbin003 <dkfeto@gmail.com>
2023-12-11 11:03:34 +0530

    module added

--- index.js
+++ index.js
@@ -4,8 +4,11 @@ const app = express();
 
 let PORT = process.env.PORT || 3000;
 
+app.get("/", (req, res) => {
+	res.json({ msg: "Hello Home!" });
+});
 app.get("/hello", (req, res) => {
-	res.json({ msg: "Hello World!!!" });
+	res.json({ msg: "Hello World!" });
 });
 
 app.listen(PORT, () => console.log(`app is running in port: ${PORT}`));
