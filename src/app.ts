cf7413ba92731f8baddd46389a338d447e145507
kobbin003 <dkfeto@gmail.com>
2023-12-11 12:03:34 +0530

    .

--- src/app.ts
+++ src/app.ts
@@ -7,6 +7,7 @@ let PORT = process.env.PORT || 3000;
 app.get("/", (req, res) => {
 	res.json({ msg: "Hello Home!" });
 });
+
 app.get("/hello", (req, res) => {
 	res.json({ msg: "Hello World!" });
 });
